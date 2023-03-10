a391ec731801d5e9b58be15934e9348cce16eeda
davidke1234 <83733928+davidke1234@users.noreply.github.com>
2023-03-10 06:18:34 -0800

    added mockito and powerMock

--- src/Tests/Test1.java
+++ src/Tests/Test1.java
@@ -1,15 +1,24 @@
 package Tests;
 
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
-
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 import CodeSmell.duplicateCodeCheck;
+import org.easymock.*;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 public class Test1 {
 
+	//@RunWith(PowerMockRunner.class)
+	//@PrepareForTest()
+	public void testDuplicateCodeCheck() {
+		duplicateCodeCheck dupes = new duplicateCodeCheck();
+	}
+	
 	@Test
 	public void testHasDupes_ReturnsTrue() {
 		duplicateCodeCheck dupes = new duplicateCodeCheck();
