8d7f5e90e630ac0b337202d548748909cca03bb6
davidke1234 <83733928+davidke1234@users.noreply.github.com>
2023-03-10 19:18:01 -0800

    add ast file and load into test

--- src/Tests/Test1.java
+++ src/Tests/Test1.java
@@ -2,26 +2,45 @@ package Tests;
 
 import static org.junit.Assert.*;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
-import CodeSmell.duplicateCodeCheck;
+
+import CodeSmell.DuplicateCodeCheck;
 import org.easymock.*;
 import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 //@RunWith(PowerMockRunner.class)
 //@PrepareForTest()
 public class Test1 {
 
-	//public void testDuplicateCodeCheck() {
-	//	duplicateCodeCheck dupes = new duplicateCodeCheck();
-	//}
+	@Test
+	public void testDuplicateCodeCheck() {
+	DuplicateCodeCheck dupes = new DuplicateCodeCheck();
+	
+	try {
+		File file = new File("CheckJava_SampleAST.txt");
+		  Scanner myReader = new Scanner(file);
+		  while (myReader.hasNextLine()) {
+		    String data = myReader.nextLine();
+		    System.out.println(data);
+		  }
+		  myReader.close();
+		} catch (FileNotFoundException e) {
+		  System.out.println("An error occurred.");
+		  e.printStackTrace();
+		}
+	}
 	
 	@Test
 	public void testHasDupes_ReturnsTrue() {
-		duplicateCodeCheck dupes = new duplicateCodeCheck();
+		DuplicateCodeCheck dupes = new DuplicateCodeCheck();
 		ArrayList<String> lines = new ArrayList<String>();
 		lines.add("String x = Hello World");
 		lines.add("String x = Hello World");
@@ -31,7 +50,7 @@ public class Test1 {
 
 	@Test
 	public void testHasDupes_ReturnFalse() {
-		duplicateCodeCheck dupes = new duplicateCodeCheck();
+		DuplicateCodeCheck dupes = new DuplicateCodeCheck();
 		ArrayList<String> lines = new ArrayList<String>();
 		lines.add("String x = Hello World");
 		boolean result = dupes.hasDupes(lines);
