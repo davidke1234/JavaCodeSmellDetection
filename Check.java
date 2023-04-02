45bf4ac9b01978849695d3cb69ba8ea65bcdfd93
davidke1234 <83733928+davidke1234@users.noreply.github.com>
2023-04-01 21:05:43 -0700

    added dup literal check

--- Check.java
+++ Check.java
@@ -1,27 +1,9 @@
 package CodeSmell;
 
 public class myClass {
-	
-	public String AA(){
+	public String ABC(){
 		String x = "hi";
-		return x;
+		String y = "hi";
+		String z = "hi";
 	}
-	public String AB(){
-		String x = "hi";
-		return x;
-	}
-	public String AC(){
-		String x = "hi";
-		return x;
-	}
-	public String AD(){
-		String x = "hi";
-		return x;
-	}
-	public String AE(){
-		String x = "hi";
-		return x;
-	}
-	
-	
 }
